f7fd60b5473f1e5901a6fe1dbb82a2219f78bbf4
skipbox <skipbox500@gmail.com>
2024-10-31 17:03:15 +0000

    3 images version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import time
 
 # Sidebar with accordion
@@ -10,23 +9,34 @@ with st.sidebar:
         button_3 = st.button("App 3", key="button_3")
 
 # Display different groups of buttons based on clicked button
-image_style = "display: block; margin: 10px auto; width: 200px; height: 200px; object-fit: cover; border: solid 2px;"
-
 if button_1:
     st.sidebar.write("Group 1")
     st.sidebar.button("Group 1 - Button A")
     st.sidebar.button("Group 1 - Button B")
     st.write("App 1")
-    st.markdown(f"<img src='https://i.imgur.com/DM1YhSh_d.webp?maxwidth=520&shape=thumb&fidelity=high' style='{image_style}' />", unsafe_allow_html=True)
+    # Display 3 images side by side using columns
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.image("https://i.imgur.com/DM1YhSh_d.webp?maxwidth=520&shape=thumb&fidelity=high", width=200)
+        st.write("Lorem ipsum dolor sit amet.")
+        st.text_area("Text Area 1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", height=100)
+    with col2:
+        st.image("https://i.imgur.com/DM1YhSh_d.webp?maxwidth=520&shape=thumb&fidelity=high", width=200)
+        st.write("Lorem ipsum dolor sit amet.")
+        st.text_area("Text Area 2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", height=100)
+    with col3:
+        st.image("https://i.imgur.com/DM1YhSh_d.webp?maxwidth=520&shape=thumb&fidelity=high", width=200)
+        st.write("Lorem ipsum dolor sit amet.")
+        st.text_area("Text Area 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", height=100)
 elif button_2:
     st.sidebar.write("Group 2")
     st.sidebar.button("Group 2 - Button C")
     st.sidebar.button("Group 2 - Button D")
     st.write("App 2")
-    st.markdown(f"<img src='https://i.imgur.com/F9LQwyA_d.webp?maxwidth=520&shape=thumb&fidelity=high' style='{image_style}' />", unsafe_allow_html=True)
+    st.image("https://i.imgur.com/DM1YhSh_d.webp?maxwidth=520&shape=thumb&fidelity=high", width=200)
 elif button_3:
     st.sidebar.write("Group 3")
     st.sidebar.button("Group 3 - Button E")
     st.sidebar.button("Group 3 - Button F")
     st.write("App 3")
-    st.markdown(f"<img src='https://i.imgur.com/2ueW1arb.jpg' style='{image_style}' />", unsafe_allow_html=True)
+    st.image("https://i.imgur.com/DM1YhSh_d.webp?maxwidth=520&shape=thumb&fidelity=high", width=200)
